f2a02b37d93505f9cc6921c58c95247b416a4dae
Christopher Wyllie <cgw@Imogen.(none)>
2012-05-22 19:37:30 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,89 @@
+
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import tornado.gen
+import tornado.escape as escape
+
+import tornadoredis
+
+REDIS_CHANNEL = 'events'
+
+class EchoWSHandler(tornado.websocket.WebSocketHandler):
+	def open(self):
+		print "WS opened"
+		self.pulseBeacon = tornado.ioloop.PeriodicCallback(self.pulse, 1000)
+		self.pulseBeacon.start()
+
+	def on_message(self, message):
+		self.write_message(u"You said: " + message)
+
+	def on_close(self):
+		print "WS closed"
+		self.pulseBeacon.stop()
+		
+	def pulse(self):
+		self.write_message(u"pulse")
+
+class RainHandler(tornado.websocket.WebSocketHandler):
+	def initialize(self, publisher):
+		self.publisher = publisher
+		self.listen()
+	
+	@tornado.gen.engine
+	def listen(self):
+		self.client = tornadoredis.Client()
+		self.client.connect()
+		yield tornado.gen.Task(self.client.subscribe, REDIS_CHANNEL)
+		self.client.listen(self.onEvent)
+	
+	def onEvent(self, message):
+		print "(rain) Handler poked from Redis"
+		if message.kind == 'message':
+			self.write_message(str(message.body))
+	
+	@tornado.gen.engine
+	def open(self):
+		print "(rain) WS opened"
+		snapshot = yield tornado.gen.Task(self.publisher.get, 'snapshot')
+		patchList = yield tornado.gen.Task(self.publisher.lrange, 'patchList', 0, -1)
+		if snapshot is not None:
+			self.write_message(escape.json_encode({
+				"event": 0,
+				"snapshot": snapshot,
+				"patchList": patchList,
+				"user": ""
+			}))
+		
+	@tornado.gen.engine
+	def on_message(self, message):
+		print "(rain) publish: " + message
+		if message[0:1] == "s":
+			print "(rain) Snapshotting..."
+			message = message[1:]
+			res = yield tornado.gen.Task(self.publisher.set, 'snapshot', message)
+			print res
+			res = yield tornado.gen.Task(self.publisher.delete, 'patchList')
+			print res
+		else:
+			yield tornado.gen.Task(self.publisher.rpush, 'patchList', message)
+		res = yield tornado.gen.Task(self.publisher.publish, REDIS_CHANNEL, message)
+		print res
+
+	def on_close(self):
+		print "(rain) WS closed"
+		self.client.unsubscribe(REDIS_CHANNEL)
+		self.client.disconnect()
+
+pubClient = tornadoredis.Client()
+pubClient.connect()
+
+application = tornado.web.Application([
+	(r"/websocket", EchoWSHandler),
+	(r"/rain", RainHandler, dict(publisher=pubClient)),
+	(r"/(.*)", tornado.web.StaticFileHandler, {"path": "./public"}),
+])
+
+if __name__ == "__main__":
+	application.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
---
+++ public/assets/js/rain.js
@@ -0,0 +1,158 @@
+
+var Rain = (function () {
+
+    var Util = {
+        uuid4: function () {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        },
+        diffMatchPatch: new diff_match_patch(),
+        log: function () {
+            if (window.console && window.console.log) {
+                console.log(Array.prototype.slice.call(arguments));
+            }
+        }
+    };
+    
+    var Event = {
+        INIT: 0,
+        PUSH_PATCH: 1,
+        PUSH_SNAPSHOT: 2
+    };
+    
+    function RainEditor (options) {
+        
+        if (options.codeMirrorArgs == undefined) {
+            options.codeMirrorArgs = {
+                lineNumbers: true,
+                fixedGutter: true
+            };
+        }
+        
+        options.diffInterval = 800;
+        
+        var codeMirror = CodeMirror.fromTextArea(
+            document.getElementById(options.textAreaId),
+            options.codeMirrorArgs
+        );
+        
+        var serverSocket = new WebSocket(options.serverURL);
+        
+        var userId = Util.uuid4();
+        
+        var lastKnownValue = "";
+        var diffInterval = null;
+        
+        var currentCursor, lineCount, newLineCount, patches, patched;
+
+        this.save = function () {
+            serverSocket.send("s"+JSON.stringify({
+                event: Event.PUSH_SNAPSHOT,
+                user: userId,
+                data: codeMirror.getValue()
+            }));
+        };
+        
+        CodeMirror.commands.save = this.save;
+        
+        function computeAndSendPatches() {
+            var currentValue = codeMirror.getValue();
+            patches = Util.diffMatchPatch.patch_make(lastKnownValue, currentValue);
+            
+            if (patches.length) {
+                serverSocket.send(JSON.stringify({
+                    event: Event.PUSH_PATCH,
+                    user: userId,
+                    data: Util.diffMatchPatch.patch_toText(patches)
+                }));
+            }
+            
+            lastKnownValue = currentValue;
+        }
+        
+        function setEditorValue(newValue) {
+            currentCursor = codeMirror.getCursor();
+                            
+            lineCount = codeMirror.lineCount();
+            codeMirror.setValue(newValue);
+            newLineCount = codeMirror.lineCount();
+            // TODO: Update for same line editing too based on line lengths?
+            if (newLineCount < lineCount) {
+                currentCursor.line -= (lineCount - newLineCount);
+            }
+            else if (newLineCount > lineCount) {
+                currentCursor.line += (newLineCount - lineCount);
+            }
+            codeMirror.setCursor(currentCursor);
+            
+            lastKnownValue = newValue;
+        }
+        
+        var diffFn = function () {
+            computeAndSendPatches();
+        };
+        
+        function startDiffing() {
+            if (diffInterval == null) {
+                diffInterval = setInterval(diffFn, options.diffInterval);
+            }
+        }
+        
+        function stopDiffing() {
+            if (diffInterval != null) {
+                clearInterval(diffInterval);
+                diffInterval = null;
+            }
+        }
+        
+        serverSocket.onmessage = function (message) {
+            message = JSON.parse(message.data);
+            
+            if (message.user != userId) {
+                stopDiffing();
+                
+                switch (message.event) {
+                    case Event.INIT:
+                        lastKnownValue = JSON.parse(message.snapshot).data;
+                        var patch;
+                        for (var i = 0; i < message.patchList.length; i++) {
+                            patch = JSON.parse(message.patchList[i]);
+                            patched = Util.diffMatchPatch.patch_apply(
+                                Util.diffMatchPatch.patch_fromText(patch.data),
+                                lastKnownValue
+                            );
+                            lastKnownValue = patched[0];
+                        }
+                        codeMirror.setValue(lastKnownValue);
+                        break;
+                    case Event.PUSH_SNAPSHOT:
+                        lastKnownValue = codeMirror.getValue();
+                        patches = Util.diffMatchPatch.patch_make(lastKnownValue, message.data); // TODO: is this back to front? - No, want to update your state (if you're behind the times?!)
+                        patched = Util.diffMatchPatch.patch_apply(patches, lastKnownValue);
+                        setEditorValue(patched[0]);
+                        break;
+                    case Event.PUSH_PATCH:
+                        computeAndSendPatches();
+                        patched = Util.diffMatchPatch.patch_apply(
+                            Util.diffMatchPatch.patch_fromText(message.data),
+                            codeMirror.getValue()
+                        );
+                        setEditorValue(patched[0]);
+                        break;
+                }
+                
+                startDiffing();
+            }
+        };
+        
+        serverSocket.onerror = function () {
+            Util.log("Socket error!");
+        };
+        
+    };
+    
+    return RainEditor;
+
+}());
